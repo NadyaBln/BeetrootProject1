41de67d9fbfcd4d1434a99560bd4233b8c9d4586
NadyaBln <nadyabln@gmail.com>
2022-03-06 18:02:32 +0200

    classwork - lesson 10 OOP

---
+++ BeetrootProject1/lesson-10-OOP/Phone Book/PhoneBookRecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_10_OOP.Phone_Book
+{
+    public class PhoneBookRecord
+    {
+        public Person Person;
+        public int Number;
+
+        public PhoneBookRecord (Person person, int number)
+        {
+            this.Person = person;
+            this.Number = number;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{this.Person.Name}, {this.Number}");
+        }
+    }
+}
