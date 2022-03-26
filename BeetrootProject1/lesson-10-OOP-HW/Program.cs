5eb007a56e4f19b42da36081381986aa5c65ff45
NadyaBln <nadyabln@gmail.com>
2022-03-26 20:34:06 +0200

    add class Author

---
+++ BeetrootProject1/lesson-10-OOP-HW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson_10_OOP_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
