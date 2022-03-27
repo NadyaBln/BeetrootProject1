00eb65d6eaa59f26718304e6662091a68864439f
NadyaBln <nadyabln@gmail.com>
2022-03-27 18:34:26 +0300

    add classes Author,Book, Library, LibraryIrem. use methods in classProgram

--- BeetrootProject1/lesson-10-OOP-HW/Program.cs
+++ BeetrootProject1/lesson-10-OOP-HW/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using lesson_10_OOP_HW.Library_Dir;
+using System;
 
 namespace lesson_10_OOP_HW
 {
@@ -6,7 +7,38 @@ namespace lesson_10_OOP_HW
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //class Author instance
+            Author firstAuthor = new();
+            firstAuthor.Country = "Georgia";
+            firstAuthor.BookAmount = 78;
+            firstAuthor.FullName = "Boris Akunin";
+
+            Author secondAuthor = new();
+            secondAuthor.FullName = "Taras Shevchenko";
+            secondAuthor.Country = "Ukraine";
+            secondAuthor.BookAmount = 105;
+
+            firstAuthor.PrintAuthor();
+            Console.WriteLine(secondAuthor.GetInfo());
+
+            //class Book instance
+            Library_Dir.Book firstBook = new("Fandorin", firstAuthor, "drama", 1997);
+            Library_Dir.Book secondBook = new("Kobzar", secondAuthor, "history", 1850);
+            
+            //Library_Dir.Book secondBook = new("Kobzar", "history", 1850);
+            //Library_Dir.LibraryItem secondBook = new(secondAuthor.FullName, secondBook.BookName);
+            Console.WriteLine(firstBook.GetInfo());
+            Console.WriteLine(secondBook.GetInfo());
+
+            var libraryItemOne = new Library_Dir.LibraryItem(firstAuthor, firstBook);
+            var libraryItrmTwo = new Library_Dir.LibraryItem(secondAuthor, secondBook);
+
+            Library item = new Library(new LibraryItem[]
+               {libraryItemOne,
+                libraryItrmTwo
+               });
+
+            item.PrintLibrary();
         }
     }
 }
