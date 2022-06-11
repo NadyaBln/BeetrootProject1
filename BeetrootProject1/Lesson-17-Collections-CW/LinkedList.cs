1c7ef247e5772e7d69475e916e4014e2acccf3af
NadyaBln <nadyabln@gmail.com>
2022-06-11 10:28:33 +0300

    not done

--- BeetrootProject1/Lesson-17-Collections-CW/LinkedList.cs
+++ BeetrootProject1/Lesson-17-Collections-CW/LinkedList.cs
@@ -4,56 +4,138 @@ using System.Collections.Generic;
 
 namespace Lesson17.Collections
 {
+   public interface IIterable<T>
+    {
+        IIterator<T> GetIterator();
+    }
+    //create interface
+    public interface IIterator<T>
+    {
+        bool HasNext();
+        T Current { get; }
+        void Reset();
+    }
+    //create interface realiation
+    class Iterator<T> : IEnumerator<T>
+    {
+        //in this method are the same as im this methods
+        //   for (int i= 0; i<list.Count; i++)
+        //{
+        //    Console.WriteLine($" {list.GetByIndex(i)}");
+        //}
+
+        private readonly LinkedList<T> _list;
+        private int _index;
+
+        public Iterator(LinkedList<T> list)
+        {
+            this._list = list;
+        }
+        
+        //($" {list.GetByIndex(i)}");
+        public T Current { get; private set; }
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        //i<list.Count
+        public bool MoveNext()
+        {
+            if(this._index < this._list.Count)
+            {
+                Current = this._list.GetByIndex(this._index++);
+                return true;
+            }
+            return false;
+        }
+
+        //same as pointer / reset -> int i= 0
+        public void Reset()
+        {
+            this._index = 0;
+        }
+    }
+
     public class LinkedList<T> : ICollection<T>
     {
+        //save Head, Tail, amount of elements of linked list
+        private Node<T> _head;
+        private Node<T> _tail;
+        //according to Incapsulation save Count as privare field
+        private int _count;
+
+        //класс, который saves ссылку на след элемент и сохраняет собственное значение
         private class Node<T>
         {
+            //constructor
             public Node(T value, Node<T> next)
             {
                 Value = value;
                 Next = next;
             }
-
+            // save собственное значение
             public T Value { get; }
 
+            //save ссылку на след элемент
             public Node<T> Next { get; set; }
         }
-
-        private Node<T> head;
-        private Node<T> tail;
-        private int _count;
-
         public void Add(T item)
         {
-            if (this.head == null)
+            //check if there is a Head
+            if (this._head == null)
             {
-                this.head = new Node<T>(item, null);
-                this.tail = this.head;
+                //if not - save new
+                //pass value - item and link to next element - null
+                this._head = new Node<T>(item, null);
+                //head is the tail now 
+                this._tail = this._head;
             }
+
             else
             {
+                //in case head is present
+                //create current element we want to save
+                //in parameters: next element = null because we don't know it
                 var newNode = new Node<T>(item, null);
-                this.tail.Next = newNode;
 
-                this.tail = newNode;
+                //we are linking new node to Tail
+                this._tail.Next = newNode;
+
+                //save new tail
+                this._tail = newNode;
             }
-
+            //increase Count 
             this._count++;
         }
 
+        //Count returns from Private variable (Back in field)
+        public int Count => this._count;
+
+
+        //return full List
         public IEnumerable<T> GetAll()
         {
-            var item = this.head;
-            while (item != null)
+            //save Head elemment 
+            var item = this._head;
+
+            //returns current item 
+            //while there are next elements
+            while (item != null) 
             {
                 yield return item.Value;
                 item = item.Next;
-            }
+            }             
         }
 
+
+        //check if in collection is certain element
         public bool Contains(T item)
         {
-            var pointer = this.head;
+            var pointer = this._head;
             while (pointer != null)
             {
                 if (pointer.Value.Equals(item))
@@ -62,8 +144,29 @@ namespace Lesson17.Collections
                 }
                 pointer = pointer.Next;
             }
-
             return false;
+        }
+
+        //find element by index
+        //returns T 
+        public T GetByIndex(int index)
+        {
+            //validation
+            if (index < 0 || index >= this._count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            //point to head (1st element)
+            var pointer = this._head;
+            //if we search 0 element - we will skip loop because i > or = index
+            for (int i = 0; i < index; i++)
+            {
+                //add index times to pointer next item
+                pointer = pointer.Next;
+            }
+            //returns 0 element
+            return pointer.Value;
         }
 
         public void CopyTo(T[] array, int arrayIndex)
@@ -73,9 +176,6 @@ namespace Lesson17.Collections
 
         public bool Remove(T item)
             => throw new NotImplementedException();
-
-        public int Count => this._count;
-
         public bool IsReadOnly { get; } = false;
 
         public void Clear()
@@ -84,7 +184,7 @@ namespace Lesson17.Collections
         }
 
         public IEnumerator<T> GetEnumerator()
-            => throw new NotImplementedException();
+            => new Iterator<T>(this);
 
         IEnumerator IEnumerable.GetEnumerator()
             => this.GetEnumerator();
--- BeetrootProject1/Lesson-17-Collections-CW/Program.cs
+++ BeetrootProject1/Lesson-17-Collections-CW/Program.cs
@@ -1,47 +1,102 @@
-﻿using Lesson_17_Collections_CW;
-using Lesson17.Collections;
-using System;
+﻿        //using Lesson_17_Collections_CW;
+        using Lesson17.Collections;
+        using System;
+using System.Collections.Generic;
+using System.Linq;
 
+        //create Linked list
+        var list = new LinkedList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
 
-var list = new LinkedList<int>();
-list.Add(1);
-list.Add(2);
-list.Add(3);
-list.Add(4);
+        //print it
+        foreach (var item in list.GetAll())
+        {
+            Console.WriteLine(item);
+        }
 
-foreach (var item in list.GetAll())
+        Console.WriteLine("-----------------------------");
+
+        //print all values from Linked list
+        for (int i= 0; i< list.Count; i++)
+        {
+            Console.WriteLine($" {list.GetByIndex(i)}");
+        }
+
+        Console.WriteLine("-----------------------------");
+
+        //create array
+        var array = Enumerable.Range(0, 10).ToArray();
+
+        //print array 
+        //replace this loop with below one
+        for (int i = 0; i<array.Length; i++)
+        {
+            Console.WriteLine("from array");
+            Console.WriteLine(array[i]);
+        }
+        Console.WriteLine("-----------------------------");
+
+//create method print to unify array and list printing
+//for it create commmon interface LinkedListIretator
+
+//same 
+//var iterator = new Iterator<int>(list);
+//IIterator<int> iterator = list.GetIterator();
+
+Console.WriteLine("-----------------------------");
+
+void Print(IEnumerator<int> iterator)
 {
-    Console.WriteLine(item);
+    //put pointer to 0
+    iterator.Reset();
+
+    //replace loop top with this
+    //while there is Next element -> print it
+    while (iterator.HasNext())
+    {
+        Console.WriteLine(iterator.Current);
+    }
 }
 
-Console.WriteLine(list.Contains(2));            // true
-Console.WriteLine(list.Contains(100));          // false
+Print(list.GetEnumerator());
 
-var stringList = new LinkedList<string>
-{
-    "sfsfd",
-    "24234"
-};
+var index = 2;
+        Console.WriteLine($"{index} item of list is {list.GetByIndex(index)}");  //2 item of list is 3
 
-foreach (var item in stringList.GetAll())
-{
-    Console.WriteLine(item);
-}
 
-var set = new Set <int>(100);
-set.Add(1);
-set.Add(2);
-set.Add(3);
-set.Add(33);
+        //use method Contains
+        Console.WriteLine(list.Contains(2));            // true
+        Console.WriteLine(list.Contains(100));          // false
 
-Console.WriteLine(set.Contains(1));
-Console.WriteLine(set.Contains(3));
-Console.WriteLine(set.Contains(33));
-Console.WriteLine(set.Contains(45));
+        //create new linked list that saves strings
+        var stringList = new LinkedList<string>
+        {
+            "sfsfd",
+            "24234"
+        };
 
-// n - кількість, що не здали       100
-// m - загальна кількість           15000
-// O(m)                             15000
-// O(m*n)                           1500000
+        foreach (var item in stringList.GetAll())
+        {
+            Console.WriteLine(item);
+        }
+
+        var set = new Set<int>(100);
+        set.Add(1);
+        set.Add(2);
+        set.Add(3);
+        set.Add(33);
+
+        Console.WriteLine(set.Contains(1));
+        Console.WriteLine(set.Contains(3));
+        Console.WriteLine(set.Contains(33));
+        Console.WriteLine(set.Contains(45));
+
+        // n - кількість, що не здали       100
+        // m - загальна кількість           15000
+        // O(m)                             15000
+        // O(m*n)                           1500000
 
 
