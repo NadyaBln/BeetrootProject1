291d0118183f4443d3dc6ee4186e4996fcdf0900
NadyaBln <nadyabln@gmail.com>
2022-06-13 20:57:13 +0300

    to continue

---
+++ BeetrootProject1/lesson-20-extensions-CW/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lesson_20_extensions_CW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str1 = "My string";
+            Console.WriteLine(WordCount(str1));
+            Console.WriteLine(str1.WordCount());
+        }
+        public static int WordCount(string str)
+        {
+            return str.Split(new char[] { ' ', '.', '?', '!' },
+                StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+    }
+
+    public static class StringExtentions
+    {
+        public static int WordCount(this string str)
+        {
+            return str.Split(new char[] { ' ', '.', '?', '!' },
+             StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
