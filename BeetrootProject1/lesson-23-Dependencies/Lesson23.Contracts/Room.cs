1df8d1539bbd915ddc5171469bdd1cb9cb70872b
NadyaBln <nadyabln@gmail.com>
2022-06-22 19:00:52 +0300

    need to continue

---
+++ BeetrootProject1/lesson-23-Dependencies/Lesson23.Contracts/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson23.Contracts
+{
+    public class Room
+    {
+        public string Name { get; set; }
+
+        public List<Meeting> Meetings { get; set; }
+    }
+}
---
+++ BeetrootProject1/lesson-23-Dependencies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson_23_Dependencies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
