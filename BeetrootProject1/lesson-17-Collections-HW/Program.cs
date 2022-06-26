5949c0d3767114d6a73b98ebff6dd46dc4c32bf9
NadyaBln <nadyabln@gmail.com>
2022-06-26 12:37:08 +0300

    HW - Vote system done

---
+++ BeetrootProject1/lesson-17-Collections-HW/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson_17_Collections_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program to ‘vote’ for anything.Via the console interface users will create a ‘vote topic’ with options.
+            //Voters will vote via console interface as well.
+            //Users can see voting results via console interface.
+
+            QuestionsAnswers voteQA = new QuestionsAnswers();
+            voteQA.CreateVoteTopic();
+
+            Vote vote = new Vote();
+            vote.MakeAVote();
+            vote.GetTheResult();
+            vote.GetStatistics();
+        }
+    }
+}
---
+++ BeetrootProject1/lesson-17-Collections-HW/QuestionsAnswers.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace lesson_17_Collections_HW
+{
+    class QuestionsAnswers
+    {
+        List<string> userQuestions = new List<string>();
+
+        public void CreateVoteTopic()
+        {
+            Console.WriteLine("Please enter a question for voting. The answers should be Yes or No");
+            var ListInput = Console.ReadLine();
+            userQuestions.Add(ListInput);
+        }
+    }
+
+    class Vote
+    {
+        List<string> users = new List<string>();
+        List<string> results = new List<string>();
+        int answerYes = 0;
+        int answerNo = 0;
+        public void MakeAVote()
+        {
+            Console.WriteLine("Hi users! Please enter your names and answer one by one. Only 3 users can make a vote.");
+
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine("Please enter your name:");
+                string userName = Console.ReadLine().ToLowerInvariant();
+                users.Add(userName);
+
+                Console.WriteLine("Please enter your answer: Yes or No");
+                string userAnswer = Console.ReadLine().ToLowerInvariant();
+                results.Add(userAnswer);
+
+                if (userAnswer == "Yes" || userAnswer == "yes") answerYes++;
+                else if (userAnswer == "No" || userAnswer == "no") answerNo++;
+                else Console.WriteLine("Invalid input");
+            }
+        }
+
+        public void GetTheResult()
+        {
+            int total = answerYes + answerNo;
+
+            double persentYes = answerYes * 100.0 / total;
+            double persentNo = answerNo * 100.0 / total;
+
+            Console.WriteLine($"Yes counts: {answerYes} - {persentYes}%");
+            Console.WriteLine($"No counts: {answerNo} - {persentNo}%");
+
+            if (answerYes > answerNo) Console.WriteLine("Answer Yes won!");
+            else if (answerNo > answerYes) Console.WriteLine("Anser No won!");
+            else Console.WriteLine("draw");
+        }
+
+        public void GetStatistics()
+        {
+            Console.WriteLine();
+            Console.WriteLine("User's votes");
+
+            IEnumerable<Tuple<string, string>> userAndRes = users.Zip(results, (user, result) => new Tuple<string, string>(user, result));
+
+            foreach (Tuple<string, string> tuple in userAndRes)
+            {
+                Console.WriteLine("{0}\t{1}", tuple.Item1, tuple.Item2);
+            }
+        }
+    }
+}
