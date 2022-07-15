e95f2ca1c6bd08d7b67aaba9649bf71fb4dacae7
NadyaBln <nadyabln@gmail.com>
2022-07-15 18:25:31 +0300

    29 - cw done

---
+++ BeetrootProject1/lesson-29-EntityFramework-CW/DataAccess/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿
+namespace lesson_29_EntityFramework_CW.DataAccess.Entities
+{
+    //products has category
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Title { get; set; }
+        public float Price { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ BeetrootProject1/lesson-29-EntityFramework-CW/DataAccess/ShopDataContext.cs
@@ -0,0 +1,35 @@
+﻿using lesson_29_EntityFramework_CW.DataAccess.Entities;
+using lesson_29_EntityFramework_CW.DataAccess.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace lesson_29_EntityFramework_CW.DataAccess
+{
+    //context = DataBase
+    class ShopDataContext : DbContext
+    {
+        //db set = table
+        //specify data type we use in each table 
+        //in table Category we use object Category
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        //mapping is discribing connection btw category and product
+        //set mapping for product and category
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+            //mapping settings for category and product
+            modelBuilder.ApplyConfiguration(new CategoryMapping());
+            modelBuilder.ApplyConfiguration(new ProductMapping());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //use Sql server with this connection string
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //connection string to DB
+            optionsBuilder.UseSqlServer(@"Server=.;Database=ShopDB;Trusted_Connection=True;");
+        }
+
+
+    }
+}
